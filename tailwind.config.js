cdeea39ad5fdf09f01a96cf85d8f59dbe8837f0c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-26 19:50:33 -0300

    feat: ✨ styles initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@
 module.exports = {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBg: 'var(--primary-background)',
+        secondaryBg: 'var(--secondary-background)',
+        primaryColor: 'var(--primary-color)',
+        secondaryColor: 'var(--secondary-color)',
+        accent: 'var(--accent)',
+        text: 'var(--text)',
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1200px',
+        xxl: '1440px',
+      },
+      fontFamily: {
+        text: ['Poppins', 'sans-serif'],
+        title: ['Chakra Petch', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
