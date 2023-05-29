bc9070737ad9a65a0dafc2745cc604e88f35ba38
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-29 09:47:19 -0300

    feat: ✨ reset counter

--- src/scripts/timeCounter.ts
+++ src/scripts/timeCounter.ts
@@ -1,6 +1,8 @@
 let initialTime = 0;
 let interval: NodeJS.Timeout;
 
+const timeElement: HTMLElement | null = document.getElementById('time-count');
+
 export const startTimer = (): void => {
   initialTime = Date.now();
   interval = setInterval(updateTimer, 1000);
@@ -10,24 +12,26 @@ export const stopTimer = (): void => {
   clearInterval(interval);
 };
 
+export const resetTimer = () => {
+  if (timeElement) {
+    timeElement.innerHTML = '00:00:00';
+  }
+};
+
 const updateTimer = (): void => {
   const tiempoActual: number = Date.now() - initialTime;
   const segundos: number = Math.floor(tiempoActual / 1000) % 60;
   const minutos: number = Math.floor(tiempoActual / (1000 * 60)) % 60;
   const horas: number = Math.floor(tiempoActual / (1000 * 60 * 60));
 
-  // Agrega ceros a la izquierda si es necesario
   const segundosTexto: string =
     segundos < 10 ? '0' + segundos : segundos.toString();
   const minutosTexto: string =
     minutos < 10 ? '0' + minutos : minutos.toString();
   const horasTexto: string = horas < 10 ? '0' + horas : horas.toString();
 
-  // Actualiza el elemento HTML con el tiempo transcurrido
-  const tiempoElemento: HTMLElement | null =
-    document.getElementById('time-count');
-  if (tiempoElemento) {
-    tiempoElemento.innerHTML =
+  if (timeElement) {
+    timeElement.innerHTML =
       horasTexto + ':' + minutosTexto + ':' + segundosTexto;
   }
 };
