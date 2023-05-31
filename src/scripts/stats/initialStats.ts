f51f9df921005171695b3fab3dae2dd0603352c7
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-05-31 19:44:41 -0300

    feat: ✨ initial stats

---
+++ src/scripts/stats/initialStats.ts
@@ -0,0 +1,37 @@
+import { Stats } from '../../types/index.js';
+const initialStats: Stats = {
+  general: {
+    gamesPlayed: 0,
+    favouriteIcon: {
+      programming: 0,
+      animals: 0,
+      sports: 0,
+    },
+    favouriteDifficulty: {
+      easy: 0,
+      normal: 0,
+      hard: 0,
+    },
+  },
+  easy: {
+    gamesPlayed: 0,
+    bestTime: '--',
+    bestMoves: '--',
+  },
+  normal: {
+    gamesPlayed: 0,
+    bestTime: '--',
+    bestMoves: '--',
+  },
+  hard: {
+    gamesPlayed: 0,
+    bestTime: '--',
+    bestMoves: '--',
+  },
+};
+
+const setInitialStats = () => {
+  localStorage.setItem('stats', JSON.stringify(initialStats));
+};
+
+export default setInitialStats;
